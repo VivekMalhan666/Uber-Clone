90761526e8b7326cd8629c49f9965a20041ace1c
fsociety <fsociety@root>
2021-08-23 15:40:47 +0530

    Back Button - Back Button to homescreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
 import { createStackNavigator } from '@react-navigation/stack';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation();
   return (
     <KeyboardAvoidingView
       behavior={`${Platform.OS == 'ios' ? 'padding' : 'height'}`}
       enabled
     >
+      <TouchableOpacity
+        onPress={() => navigation.navigate('HomeScreen')}
+        style={tw`absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg bg-gray-100`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
